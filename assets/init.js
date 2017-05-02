2626e7fb106d29c400f7946211dc74a2014e2938
Yen-Chieh Chen <yenchieh@nulab-inc.com>
2017-05-02 16:25:39 -0400

    Add searchImages function to get images from pixabay.com

--- assets/init.js
+++ assets/init.js
@@ -1,7 +1,13 @@
 
 $(function(){
-    $('.jsSearch').on("click", function() {
-
+    var $input = $('.jsSearchInput');
+    $('.jsSearch').on("click", function(e) {
+        e.preventDefault();
+        search($input.val());
     });
+
+    function search(query) {
+        window.location.href="/search?q=" + query;
+    }
 });
 
--- main.go
+++ main.go
@@ -3,16 +3,51 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"net/url"
+
+	"encoding/json"
+
+	"io/ioutil"
+
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-type PageValues struct {
-	Title string
-	Body  string
+type PixbayImageResponse struct {
+	TotalHits   int64
+	SearchQuery string
+	Hits        []ImageDetail
 }
 
+type ImageDetail struct {
+	PreviewHeight   int64
+	Likes           int64
+	Favorites       int64
+	Tags            string
+	WebformatHeight int64
+	PreviewWidth    int64
+	Comments        int64
+	downloads       int64
+	PageURL         string
+	PreviewURL      string
+	WebformateURL   string
+	ImageWidth      int64
+	UserID          int64
+	User            string
+	Type            string
+	ID              int64
+	UserImageURL    string
+	ImageHeight     int64
+}
+
+var key string
+
 func main() {
+
+	fmt.Printf("%#v", os.Args)
+	key = os.Args[1]
 
 	r := gin.Default()
 
@@ -22,28 +57,62 @@ func main() {
 	r.NoRoute(notFoundPage)
 
 	r.GET("/", home)
-	r.GET("/searchGopher", getGopher)
+	r.GET("/search", searchImages)
 
 	r.Run(":9999")
 }
 
 func home(c *gin.Context) {
-
-	pageValues := PageValues{
-		Title: "Bowery Golang Demo",
-		Body:  "May 4th be with you",
-	}
-
-	c.HTML(http.StatusOK, "home.html", pageValues)
+	c.HTML(http.StatusOK, "home.html", nil)
 }
 
-func getGopher(c *gin.Context) {
+func searchImages(c *gin.Context) {
 	URL, err := url.Parse("https://pixabay.com/api")
 	if err != nil {
 		c.Redirect(http.StatusTemporaryRedirect, "/404")
 		return
 	}
 
+	q := c.Query("q")
+	if q == "" {
+		q = "gopher"
+	}
+	var req *http.Request
+	form := &url.Values{
+		"key":  {key},
+		"q":    {q},
+		"lang": {"en"},
+	}
+
+	urlWithQuery := fmt.Sprintf("%s?%s", URL.String(), form.Encode())
+
+	req, err = http.NewRequest(http.MethodGet, urlWithQuery, nil)
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var images PixbayImageResponse
+
+	err = json.Unmarshal(body, &images)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	images.SearchQuery = q
+
+	c.HTML(http.StatusOK, "home.html", images)
 }
 
 func notFoundPage(c *gin.Context) {
